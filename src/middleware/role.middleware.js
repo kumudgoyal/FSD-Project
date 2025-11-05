c88a6c5fcc24bbcfb9dd664cd1cb4c470863279a
kumudgoyal <162026111+kumudgoyal@users.noreply.github.com>
2025-11-05 18:17:19 +0530

    Delete src directory

--- src/middleware/role.middleware.js
+++
@@ -1,8 +0,0 @@
-export const checkRole = (...allowedRoles) => {
-  return (req, res, next) => {
-    if (!req.user || !allowedRoles.includes(req.user.role)) {
-      return res.status(403).json({ message: "Access denied" });
-    }
-    next();
-  };
-};
